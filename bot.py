7ff0336094ce6d45902c4c6dd48c4eae05baf644
bakliaissa916-lgtm <bakliaissa916@gmail.com>
2026-04-24 16:10:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -390,3 +390,18 @@ while True:
     except Exception as e:
         print(e)
         
+        from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot alive"
+
+def run_web():
+    port=int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port)
+
+threading.Thread(target=run_web).start()
