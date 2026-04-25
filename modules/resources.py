cf0796914e0f699392a1a2750edc60f367f7bc83
bakliaissa916-lgtm <bakliaissa916@gmail.com>
2026-04-25 17:46:39 +0100

    Create resources.py

---
+++ modules/resources.py
@@ -0,0 +1,17 @@
+tips=[
+"الفراغ أخطر من الشهوة",
+"غيّر البيئة عند الخطر",
+"ابدأ بخطوة صغيرة"
+]
+
+books=[
+"Atomic Habits",
+"Deep Work",
+"Your Brain On Porn"
+]
+
+sites=[
+"مشروع واعي",
+"FightTheNewDrug",
+"YourBrainOnPorn"
+]
